43f67d3cfbf1cb846fbc390caae82bb1dac245bd
0x600d1dea <xiaobin.huang@gmail.com>
2017-04-26 23:45:52 +0800

    upgrade Unity3D.Lua

--- fraxy_m/Assets/StreamingAssets/LuaRoot/Timer.lua
+++ fraxy_m/Assets/StreamingAssets/LuaRoot/Timer.lua
@@ -1,16 +1,34 @@
+
+-- https://blog.acolyer.org/2015/11/23/hashed-and-hierarchical-timing-wheels/
+-- Scheme 6
+
 local Timer = {}
 
-function Timer:After(seconds, event)
+local kMaxIntervalShift = 8  --> 1 << kMaxIntervalShift seconds
+local kMaxSlots = 1 << kMaxIntervalShift
+local kSlotsMask = kMaxSlots - 1
+local origin = os.clock()
+local pointer = 1
+local round = 0
+local slots = {}
+for i = 1, kNumBuckets do
+	slots[i] = {}
+end
+
+function Timer.After(seconds, event)
+	local s = math.floor(s)
+	local c = s >> kMaxIntervalShift
+	local deltaSlot = s - c << kMaxIntervalShift
+	local slot = (pointer + deltaSlot) & kSlotsMask
+	local delta = seconds - s
+	local events = slots[slot]
+	events[#events + 1] = { c, delta, event}
+end
+
+function Timer.Update()
+	local cur = os.clock()
 	
 
 end
-
-
-
-function Timer:Update()
-
-end
-
-
 
 return Timer
